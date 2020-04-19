06219820df8856607c3289049dd1ed7b66f2afd3
Khloud Mohamed Abd El-Aal <k.abdelaal@mcsoil.com>
2020-04-19 15:46:34 +0200

    initial commit

---
+++ Supermarket/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+
+namespace Supermarket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXWindow
+    {
+         List <clsInvoice> InvoiceItems  = new List <clsInvoice>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btCash_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btTransaction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReturn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHoldInv1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPriceCheck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHoldInv2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDiscLinePercent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVoidLine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDiscLineValue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVoidAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnQuick1_Click(object sender, RoutedEventArgs e)
+        {
+            clsInvoice item1 = new clsInvoice();
+            item1.Name = "milk";
+            item1.Price = 13;
+            InvoiceItems.Add(item1);
+            grdInvoice.ItemsSource = InvoiceItems;
+            grdInvoice.RefreshData();
+        }
+    }
+}
---
+++ Supermarket/clsInvoice.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    class clsInvoice
+    {
+        private string _name ;
+        private Int32 _qty ;
+        private Int32 _price;
+        private Int16 _discount;
+        private Int32 _total;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public Int32 Qty
+        {
+            get
+            {
+                return _qty;
+            }
+            set
+            {
+                _qty = value;
+            }
+        }
+        public Int32 Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = value;
+            }
+        }
+        public Int16 Discount
+        {
+            get
+            {
+                return _discount;
+            }
+            set
+            {
+                _discount = value;
+            }
+        }
+        public Int32 Total
+        {
+            get
+            {
+                return _total = Price*Qty;
+            }
+            set
+            {
+                _total = value;
+            }
+        }
+       
+    }
+}
